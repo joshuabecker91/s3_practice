ab6a5da83de4af4071a503572d16cd91b15e0249
joshuabecker91 <joshua.becker91@gmail.com>
2022-12-18 10:07:26 -0800

    updates

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 "# s3_practice" 
 
-S3 Bucket Practice using python and boto3
+AWS S3 Bucket Practice using python and boto3
+Technologies used: Python, boto3, AWS S3, requests, python-dotenv
 
-Technologies used: Python, AWS S3, boto3, requests, python-dotenv
+AWS S3 Bucket Practice using javascript w/node
+Technologies used: JavaScript, Node, AWS S3, aws-sdk, dotenv
--- s3_practice_python.py
+++ s3_practice_python.py
@@ -97,3 +97,100 @@ obj.delete()
 # with open("./burger.jpg", "rb") as f:
 #     s3.put_object
 
+
+
+
+
+
+
+
+# NOTES below--------------------------------------------------------------------------
+
+# BUCKET_NAME = "boto-tutorial-bucket"
+
+# s3 = boto3.client("s3")
+
+# # List all buckets
+# buckets_resp = s3.list_buckets()
+# for bucket in buckets_resp["Buckets"]:
+#     print(bucket)
+
+# # List all objects in a bucket
+# response = s3.list_objects_v2(Bucket=BUCKET_NAME)
+# for obj in response["Contents"]:
+#     print(obj)
+
+# # Upload file to bucket [Show with public-read, and without it too]
+# with open("./burger.jpg", "rb") as f:
+#     print(f.name)
+#     s3.upload_fileobj(f, BUCKET_NAME, "burger.jpg", ExtraArgs={"ACL": "public-read"})
+
+# # Download File
+# s3.download_file(BUCKET_NAME, "burger.jpg", "downloaded_burger.jpg")
+
+# # Download File with reference
+# with open("downloaded_burger.jpg", "wb") as f:
+#     s3.download_fileobj(BUCKET_NAME, "burger.jpg", f)
+
+# # Presigned URL to give limited access to an unauthorized user
+# url = s3.generate_presigned_url(
+#     "get_object", Params={"Bucket": BUCKET_NAME, "Key": "burger.jpg"}, ExpiresIn=30
+# )
+# print(url)
+
+
+# # Create Bucket
+# bucket_location = s3.create_bucket(ACL="public-read", Bucket="new-copy-destination-123")
+# print(bucket_location)
+
+# # Copy object
+# response = s3.copy_object(
+#     ACL="public-read",
+#     Bucket="new-copy-destination-123",
+#     CopySource=f"/{BUCKET_NAME}/burger.jpg",
+#     Key="CopiedBurger.jpg",
+# )
+# print(response)
+
+# # Get Object
+# response = s3.get_object(Bucket=BUCKET_NAME, Key="burger.jpg")
+# print(response)
+
+
+
+
+
+
+
+
+# If using a public S3 - no key required, must need unsigned- ------------------------------------------------------------------
+
+# Making a bucket public
+# {
+#     "Version": "2012-10-17",
+#     "Id": "Policy1664768163631",
+#     "Statement": [
+#         {
+#             "Sid": "Stmt1664768156196",
+#             "Effect": "Allow",
+#             "Principal": "*",
+#             "Action": "s3:GetObject",
+#             "Resource": "arn:aws:s3:::s3practiceboto3/*"
+#         }
+#     ]
+# }
+
+# from botocore import UNSIGNED
+# from botocore.client import Config
+# s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED)) # public s3
+
+
+# , region_name = 'us-west-2'
+
+# s3.connect_s3()
+
+
+# Creating the low level functional client
+# client = boto3.client(
+#     's3', aws_access_key_id = 'AKIA46SFIWN5AMWMDQVB', aws_secret_access_key = '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', region_name = 'ap-south-1'
+# )
